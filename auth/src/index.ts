a34a7242b847eee60333d1776ab162fdf1372f9e
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-07-30 20:25:46 -0500

    reconfig git

---
+++ auth/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+
+const app = express();
+app.use(express.json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.listen(3000, () => {
+  console.log('[Auth Server] listening on port 3000!!!!!');
+});
