4d90a3aa631230b4c9a609c145bc534dab4eb7af
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-08-01 13:04:55 -0500

    send back cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
@@ -9,7 +10,20 @@ import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
 
 const app = express();
+app.set('trust proxy', true); // Trust traffic even though its coming from ingress nginx proxy
 app.use(express.json());
+app.use(
+  // We are going to disable encryption (signed: false) of the cookie itsself so
+  // that it can easily be understood by other services that may be written
+  // in a different language and doesn't have access to Nodes 'cookie session' and
+  // whatever encryption algorithm they use.
+  // The JWT that will be inside the cookie should be tamper proof and shouldn't contain
+  // sensitive information
+  cookieSession({
+    signed: false,
+    secure: true, // use only on HTTPS
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -23,6 +37,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -35,6 +36,19 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJWT = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // Store it on the session object
+    req.session = {
+      jwt: userJWT,
+    };
 
     res.status(201).send(user);
   }
